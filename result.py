933a70bb06c417cedcf70a5783b083dd1af790b8
paulavolane <83758783+paulavolane@users.noreply.github.com>
2024-01-18 19:53:58 +0200

    Update result.py

--- result.py
+++ result.py
@@ -2,23 +2,29 @@ import requests
 import bs4
 from PyPDF2 import PdWriter
 
-url=""
-saturs=requests.get(url)
+def info(url):
+  saturs=requests.get(url)
+  
+  if saturs.status_code==200:
+    lapa=bs4.BeautifulSoup(saturs.content, 'html.parser')
+  
+    nosaukums=lapa.find('h1', class_='header--title').text.strip()
+    sastavdalas=[sastavdalas.text.strip() for sastavdalas in lapa.find_all('span', class_='ingredient__unit')]
+    pagatavosana=[pagatavosana.text.strip() for pagatavosana in lapa.find_all('span', class_='step__text text')]
+  
+    recepte={
+      'nosaukums':nosaukums,
+      'sastavdalas':sastavdalas,
+      'pagatavosana':pagatavosana,
+    }
+  
+  else:
+    print('Šodien pietiks ar ūdens un gaiss :)')
 
-if saturs.status_code==200:
-  lapa=bs4.BeautifulSoup(saturs.content, 'html.parser')
+recepte_url=''
+recepte=info(recepte_url)
 
-  nosaukums=lapa.find('h1', class_='svelte-1muv3s8').text.strip()
-  sastavdalas=[sastavdalas.text.strip() for sastavdalas in lapa.find_all('span', class_='ingredient-text svelte-1dqq0pw')]
-  daudzums=[daudzums.text.strip() for daudzums in lapa.find_all('span', class_='ingredient-quantity svelte-1dqq0pw')]
-  pagatavosana=[pagatavosana.text.strip() for pagatavosana in lapa.find_all('li', class_='direction svelte-1dqq0pw')]
+pdfka=PdfWriter()
 
-  recepte={
-    'nosaukums':nosaukums,
-    'daudzums':daudzums,
-    'sastavdalas':sastavdalas,
-    'pagatavosana':pagatavosana,
-  }
-
-else:
-  print('Šodien pietiks ar ūdens un gaiss :)')
+with open('recepte.pdf', 'wb') as f:
+  pdfka.write(f)
