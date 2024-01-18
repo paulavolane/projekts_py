ef69c27f92201aed23d3d1931b2805bb28912fff
paulavolane <83758783+paulavolane@users.noreply.github.com>
2024-01-18 14:45:11 +0200

    Update result.py

--- result.py
+++ result.py
@@ -1,5 +1,6 @@
 import requests
 import bs4
+from PyPDF2 import PdWriter
 
 url=""
 saturs=requests.get(url)
@@ -8,7 +9,16 @@ if saturs.status_code==200:
   lapa=bs4.BeautifulSoup(saturs.content, 'html.parser')
 
   nosaukums=lapa.find('h1', class_='svelte-1muv3s8').text.strip()
-  
+  sastavdalas=[sastavdalas.text.strip() for sastavdalas in lapa.find_all('span', class_='ingredient-text svelte-1dqq0pw')]
+  daudzums=[daudzums.text.strip() for daudzums in lapa.find_all('span', class_='ingredient-quantity svelte-1dqq0pw')]
+  pagatavosana=[pagatavosana.text.strip() for pagatavosana in lapa.find_all('li', class_='direction svelte-1dqq0pw')]
+
+  recepte={
+    'nosaukums':nosaukums,
+    'daudzums':daudzums,
+    'sastavdalas':sastavdalas,
+    'pagatavosana':pagatavosana,
+  }
 
 else:
   print('Šodien pietiks ar ūdens un gaiss :)')
